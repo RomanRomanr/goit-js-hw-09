6a0cdc497bcf8410d0f1a2c92f204d636be5b0c3
Roman <romario.higa@gmail.com>
2026-02-19 17:40:53 +0100

    3-commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,14 +4,6 @@ const formData = {
     email: "",
     message: ""
 }
-form.addEventListener('input', e => {
-    const { name, value } = e.target;
-    if (name in formData) {
-        formData[name] = value.trim();
-    }
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-})
-
 const savedData = localStorage.getItem('feedback-form-state');
 if (savedData !== null) {
     const newSavedData = JSON.parse(savedData);
@@ -21,18 +13,23 @@ if (savedData !== null) {
     formData.email = newSavedData.email;
     formData.message = newSavedData.message;
 }
-
 form.addEventListener('submit', e => {
     e.preventDefault();
     if (formData.email === "" || formData.message ==="") {
         alert('Fill please all fields');
         return;
     }
-
     console.log(formData);
     localStorage.removeItem('feedback-form-state');
     formData.email = "";
     formData.message = "";
     form.reset();
-    
 })
+form.addEventListener('input', e => {
+    const { name, value } = e.target;
+    if (name in formData) {
+        formData[name] = value.trim();
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+})
+
